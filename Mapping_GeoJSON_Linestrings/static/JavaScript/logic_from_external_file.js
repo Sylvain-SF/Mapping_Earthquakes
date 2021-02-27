6cc8f3f2846e4b41f66772676670baf7c1de4962
Sylvain-SF <sylvain.dessagnes@gmail.com>
2021-02-26 16:33:38 -0800

    Fix

--- Mapping_GeoJSON_Linestrings/static/JavaScript/logic_from_external_file.js
+++ Mapping_GeoJSON_Linestrings/static/JavaScript/logic_from_external_file.js
@@ -28,7 +28,7 @@ let map = L.map('mapid', {
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL
-let torontoData= "https://raw.githubusercontent.com/Sylvain-SF/Mapping_Earthquakes/main/torontoRoutes.json"
+let torontoData= "https://raw.githubusercontent.com/Sylvain-SF/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/Mapping_GeoJSON_Linestrings/torontoRoutes.json"
 
 // Create a style for the lines.
 let myStyle = {
