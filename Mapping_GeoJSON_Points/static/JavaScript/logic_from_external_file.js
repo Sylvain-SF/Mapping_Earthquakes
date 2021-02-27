8ba59d5ffe0a98371af06e78167c99b8ecc5cb66
Sylvain-SF <sylvain.dessagnes@gmail.com>
2021-02-26 16:28:03 -0800

    Fix

--- Mapping_GeoJSON_Points/static/JavaScript/logic_from_external_file.js
+++ Mapping_GeoJSON_Points/static/JavaScript/logic_from_external_file.js
@@ -28,7 +28,7 @@ let map = L.map('mapid', {
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/Sylvain-SF/Mapping_Earthquakes/main/Data/majorAirports.json"
+let airportData = "https://raw.githubusercontent.com/Sylvain-SF/Mapping_Earthquakes/Mapping_GeoJSON_Points/Data/majorAirports.json"
 
 // Grabbing our GeoJSON data.
 d3.json(airportData).then(function(data) {
